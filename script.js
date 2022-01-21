510587c01e3de4b54a5d95be61f27a2186cd6b71
breezeonline <81129872+breezeonline@users.noreply.github.com>
2022-01-21 14:19:28 -0700

    Final Updates
    Add property names
    Remove dragging

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 /*Load map*/
-mapboxgl.accessToken = 'KEY';
-
-
-
+mapboxgl.accessToken = 'KEY HERE';
 
 /**
  * Add the map to the page
@@ -12,8 +9,10 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/light-v10',
   center: [-77.034084142948, 38.909671288923],
   zoom: 13,
-  scrollZoom: false
+  scrollZoom: false,
+  dragPan: false,
 });
+
 
 map.on('load', function () {
   map.resize();
@@ -29,6 +28,7 @@ const stores = {
         'coordinates': [-77.034084142948, 38.909671288923]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(202) 234-7336',
         'phone': '2022347336',
         'address': '1471 P St NW',
@@ -46,6 +46,7 @@ const stores = {
         'coordinates': [-77.049766, 38.900772]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(202) 507-8357',
         'phone': '2025078357',
         'address': '2221 I St NW',
@@ -63,6 +64,7 @@ const stores = {
         'coordinates': [-77.043929, 38.910525]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(202) 387-9338',
         'phone': '2023879338',
         'address': '1512 Connecticut Ave NW',
@@ -80,6 +82,7 @@ const stores = {
         'coordinates': [-77.0672, 38.90516896]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(202) 337-9338',
         'phone': '2023379338',
         'address': '3333 M St NW',
@@ -97,6 +100,7 @@ const stores = {
         'coordinates': [-77.002583742142, 38.887041080933]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(202) 547-9338',
         'phone': '2025479338',
         'address': '221 Pennsylvania Ave SE',
@@ -114,6 +118,7 @@ const stores = {
         'coordinates': [-76.933492720127, 38.99225245786]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'address': '8204 Baltimore Ave',
         'city': 'College Park',
         'country': 'United States',
@@ -128,6 +133,7 @@ const stores = {
         'coordinates': [-77.097083330154, 38.980979]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(301) 654-7336',
         'phone': '3016547336',
         'address': '4831 Bethesda Ave',
@@ -145,6 +151,7 @@ const stores = {
         'coordinates': [-77.359425054188, 38.958058116661]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(571) 203-0082',
         'phone': '5712030082',
         'address': '11935 Democracy Dr',
@@ -162,6 +169,7 @@ const stores = {
         'coordinates': [-77.10853099823, 38.880100922392]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(703) 522-2016',
         'phone': '7035222016',
         'address': '4075 Wilson Blvd',
@@ -179,6 +187,7 @@ const stores = {
         'coordinates': [-75.28784, 40.008008]
       },
       'properties': {
+        'propertyName': 'Breeze Online',
         'phoneFormatted': '(610) 642-9400',
         'phone': '6106429400',
         'address': '68 Coulter Ave',
