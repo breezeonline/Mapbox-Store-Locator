b9d801295c5ea6d1b510d8cebbf07894814bee25
breezeonline <81129872+breezeonline@users.noreply.github.com>
2022-01-23 16:22:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYnJlZXplb25saW5lIiwiYSI6ImNreHRyZXM2dDVvbGQyd
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-77.034084142948, 38.909671288923],
-  zoom: 13,
+  center: [-114.065465, 51.046095],
+  zoom: 5,
   scrollZoom: false,
   dragPan: false,
 });
@@ -24,213 +24,121 @@ const stores = {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.034084142948, 38.909671288923]
+        'coordinates': [-112.801568, 49.683748]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 234-7336',
-        'phone': '2022347336',
-        'address': '1471 P St NW',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'at 15th St NW',
-        'postalCode': '20005',
-        'state': 'D.C.'
+        'propertyName': 'Location 1',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '2727 South Parkside Dr',
+        'city': 'Lethbridge',
+        'country': 'Canada',
+        'postalCode': 'T1K 0C5',
+        'province': 'AB'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.049766, 38.900772]
+        'coordinates': [-114.089133, 51.005123]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 507-8357',
-        'phone': '2025078357',
-        'address': '2221 I St NW',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'at 22nd St NW',
-        'postalCode': '20037',
-        'state': 'D.C.'
+        'propertyName': 'Location 2',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '5900 Elbow Dr SW',
+        'city': 'Calgary',
+        'country': 'Canada',
+        'postalCode': 'T2V 1H7',
+        'province': 'AB'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.043929, 38.910525]
+        'coordinates': [-113.493043, 53.468986]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 387-9338',
-        'phone': '2023879338',
-        'address': '1512 Connecticut Ave NW',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'at Dupont Circle',
-        'postalCode': '20036',
-        'state': 'D.C.'
+        'propertyName': 'Location 3',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '3414 Gateway Boulevard',
+        'city': 'Edmonton',
+        'country': 'Canada',
+        'postalCode': 'T6J 6Z2',
+        'province': 'AB'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.0672, 38.90516896]
+        'coordinates': [-113.839474, 52.263627]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 337-9338',
-        'phone': '2023379338',
-        'address': '3333 M St NW',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'at 34th St NW',
-        'postalCode': '20007',
-        'state': 'D.C.'
+        'propertyName': 'Location 4',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '4500 Fountain Drive',
+        'city': 'Red Deer',
+        'country': 'Canada',
+        'postalCode': 'T4N 6W8',
+        'province': 'AB'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.002583742142, 38.887041080933]
+        'coordinates': [-122.989773, 49.201111]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 547-9338',
-        'phone': '2025479338',
-        'address': '221 Pennsylvania Ave SE',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'btwn 2nd & 3rd Sts. SE',
-        'postalCode': '20003',
-        'state': 'D.C.'
+        'propertyName': 'Location 5',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '9001 Riverway Pl.',
+        'city': 'Burnaby',
+        'country': 'Canada',
+        'postalCode': 'V5J 3J1',
+        'province': 'BC'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-76.933492720127, 38.99225245786]
+        'coordinates': [-123.05177, 49.213855]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'address': '8204 Baltimore Ave',
-        'city': 'College Park',
-        'country': 'United States',
-        'postalCode': '20740',
-        'state': 'MD'
+        'propertyName': 'Location 6',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': '7800 Vivian Drive Vancouver',
+        'city': 'Vancouver',
+        'country': 'Canada',
+        'postalCode': 'V5S 2V9',
+        'province': 'BC'
       }
     },
     {
       'type': 'Feature',
       'geometry': {
         'type': 'Point',
-        'coordinates': [-77.097083330154, 38.980979]
+        'coordinates': [-104.775245, 50.979099]
       },
       'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(301) 654-7336',
-        'phone': '3016547336',
-        'address': '4831 Bethesda Ave',
-        'cc': 'US',
-        'city': 'Bethesda',
-        'country': 'United States',
-        'postalCode': '20814',
-        'state': 'MD'
+        'propertyName': 'Location 7',
+        'phoneFormatted': '(123)-456-7890',
+        'phone': '1234567890',
+        'address': 'Longlaketon 219',
+        'city': 'Saskatchewan',
+        'country': 'Canada',
+        'postalCode': 'S0G 1J0',
+        'province': 'SK'
       }
     },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [-77.359425054188, 38.958058116661]
-      },
-      'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(571) 203-0082',
-        'phone': '5712030082',
-        'address': '11935 Democracy Dr',
-        'city': 'Reston',
-        'country': 'United States',
-        'crossStreet': 'btw Explorer & Library',
-        'postalCode': '20190',
-        'state': 'VA'
-      }
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [-77.10853099823, 38.880100922392]
-      },
-      'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(703) 522-2016',
-        'phone': '7035222016',
-        'address': '4075 Wilson Blvd',
-        'city': 'Arlington',
-        'country': 'United States',
-        'crossStreet': 'at N Randolph St.',
-        'postalCode': '22203',
-        'state': 'VA'
-      }
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [-75.28784, 40.008008]
-      },
-      'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(610) 642-9400',
-        'phone': '6106429400',
-        'address': '68 Coulter Ave',
-        'city': 'Ardmore',
-        'country': 'United States',
-        'postalCode': '19003',
-        'state': 'PA'
-      }
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [-75.20121216774, 39.954030175164]
-      },
-      'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(215) 386-1365',
-        'phone': '2153861365',
-        'address': '3925 Walnut St',
-        'city': 'Philadelphia',
-        'country': 'United States',
-        'postalCode': '19104',
-        'state': 'PA'
-      }
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [-77.043959498405, 38.903883387232]
-      },
-      'properties': {
-        'propertyName': 'Breeze Online',
-        'phoneFormatted': '(202) 331-3355',
-        'phone': '2023313355',
-        'address': '1901 L St. NW',
-        'city': 'Washington DC',
-        'country': 'United States',
-        'crossStreet': 'at 19th St',
-        'postalCode': '20036',
-        'state': 'D.C.'
-      }
-    }
   ]
 };
 
